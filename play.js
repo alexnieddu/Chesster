aab776cf055d62653c2f104f02855ba42e46dd7e
Alex Nieddu <alexander1.nieddu@st.oth-regensburg.de>
2019-01-06 13:32:09 +0100

    Add files via upload

---
+++ play.js
@@ -0,0 +1,276 @@
+console.log("Chesster the chess bot");
+
+var bot = require("./bot");
+
+var Twit = require("twit");
+var Chess = require("./chess.js").Chess;
+var chess = new Chess();
+var fs = require("fs");
+var config = require("./config");
+
+var T = new Twit(config)
+
+
+play();
+setInterval(play, 1000*60*15); // 1/4 hr
+
+function play() {
+	bot.getMove();
+	if(!chess.game_over()) {
+		fs.readFile("dat",  function (err, data) {
+			var ownMove = data.toString();
+			var legal_moves = chess.moves();
+			console.log("Possible Moves: " + legal_moves);
+			console.log("Your MOVE: " + ownMove);
+			if(legal_moves.includes(ownMove)) {
+				// Move of community
+				chess.move(ownMove);
+				
+				if(!chess.game_over()) {
+					var moves = chess.moves();
+				
+					// ---DIFFERENT ALGORITHMS FOR CHESSTERS MOVE---
+					// 1. Chesster's move random
+// 					var move = moves[Math.floor(Math.random() * moves.length)];
+				
+					// 2. Chesster's move capture piece
+					var move = null;
+					//use any negative large number
+					var bestValue = -9999;
+ 
+					for (var i = 0; i < moves.length; i++) {
+						var newGameMove = moves[i];
+						chess.move(newGameMove);
+ 
+						//take the negative as AI plays as black
+						var board = chess.board();
+						var boardValue = -evaluateBoard(board);
+						chess.undo();
+						if (boardValue > bestValue) {
+							bestValue = boardValue;
+							move = newGameMove
+						}
+					}
+
+					// 3. Minmax
+					// var minmax_move = minimaxRoot(3, chess, true);
+// 					console.log(minimax_move);
+					// var move = chess.to_san(move).san;
+// 				
+					// Do the move
+					chess.move(move);
+					console.log("Chesster's move: " + move);
+			
+					console.log(chess.ascii());
+// 					fs.writeFile("hist", chess.history(), function(err, data) { if(err) { console.log(err); }});
+// 					// Post to twitter
+// 					post("Community's move: " + ownMove + "\nChesster's move: " + move);
+				} else {
+					console.log("--------Game is over");
+					// Post to twitter
+					post("Game is over.");
+				}
+				
+			} else {
+				if(ownMove != "") {
+					console.log( ownMove + " is no legal move.");
+					// Post to twitter
+					// post( ownMove + " is no legal move. Comment for another move here.");
+				}
+			}
+		});
+		
+	} else {
+		console.log("--------Game is over");
+		// Post to twitter
+		post("Game is over.");
+	}
+}
+
+// 
+var minimaxRoot = function(depth, game, isMaximisingPlayer) {
+
+    var newGameMoves = game.ugly_moves();
+    var bestMove = -9999;
+    var bestMoveFound;
+
+    for(var i = 0; i < newGameMoves.length; i++) {
+        var newGameMove = newGameMoves[i]
+        game.ugly_move(newGameMove);
+        var value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
+        game.undo();
+        if(value >= bestMove) {
+            bestMove = value;
+            bestMoveFound = newGameMove;
+        }
+    }
+    return bestMoveFound;
+};
+
+var minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
+    //positionCount++;
+    if (depth === 0) {
+        return -evaluateBoard(game.board());
+    }
+
+    var newGameMoves = game.ugly_moves();
+
+    if (isMaximisingPlayer) {
+        var bestMove = -9999;
+        for (var i = 0; i < newGameMoves.length; i++) {
+            game.ugly_move(newGameMoves[i]);
+            bestMove = Math.max(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+            game.undo();
+            alpha = Math.max(alpha, bestMove);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+        return bestMove;
+    } else {
+        var bestMove = 9999;
+        for (var i = 0; i < newGameMoves.length; i++) {
+            game.ugly_move(newGameMoves[i]);
+            bestMove = Math.min(bestMove, minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer));
+            game.undo();
+            beta = Math.min(beta, bestMove);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+        return bestMove;
+    }
+};
+
+var evaluateBoard = function (board) {
+    var totalEvaluation = 0;
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            totalEvaluation = totalEvaluation + getPieceValue(board[i][j], i ,j);
+        }
+    }
+    return totalEvaluation;
+};
+
+var reverseArray = function(array) {
+    return array.slice().reverse();
+};
+
+var pawnEvalWhite =
+    [
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+        [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+        [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+        [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+        [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+        [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+        [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+    ];
+
+var pawnEvalBlack = reverseArray(pawnEvalWhite);
+
+var knightEval =
+    [
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+        [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+        [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+        [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+        [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+        [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+        [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+    ];
+
+var bishopEvalWhite = [
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+];
+
+var bishopEvalBlack = reverseArray(bishopEvalWhite);
+
+var rookEvalWhite = [
+    [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+];
+
+var rookEvalBlack = reverseArray(rookEvalWhite);
+
+var evalQueen =
+    [
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+];
+
+var kingEvalWhite = [
+
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0 ],
+    [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0 ]
+];
+
+var kingEvalBlack = reverseArray(kingEvalWhite);
+
+
+
+
+var getPieceValue = function (piece, x, y) {
+    if (piece === null) {
+        return 0;
+    }
+    var getAbsoluteValue = function (piece, isWhite, x ,y) {
+        if (piece.type === 'p') {
+            return 10 + ( isWhite ? pawnEvalWhite[y][x] : pawnEvalBlack[y][x] );
+        } else if (piece.type === 'r') {
+            return 50 + ( isWhite ? rookEvalWhite[y][x] : rookEvalBlack[y][x] );
+        } else if (piece.type === 'n') {
+            return 30 + knightEval[y][x];
+        } else if (piece.type === 'b') {
+            return 30 + ( isWhite ? bishopEvalWhite[y][x] : bishopEvalBlack[y][x] );
+        } else if (piece.type === 'q') {
+            return 90 + evalQueen[y][x];
+        } else if (piece.type === 'k') {
+            return 900 + ( isWhite ? kingEvalWhite[y][x] : kingEvalBlack[y][x] );
+        }
+        throw "Unknown piece type: " + piece.type;
+    };
+
+    var absoluteValue = getAbsoluteValue(piece, piece.color === 'w', x ,y);
+    return piece.color === 'w' ? absoluteValue : -absoluteValue;
+};
+
+
+// Post a tweet
+function post(msg) {
+	T.post('statuses/update', { status: msg }, function (err, data, response) {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log("\n Finished!\n");
+				//console.log(data);
+			}
+		});
+}
---
+++ twitter.js
@@ -0,0 +1,134 @@
+console.log("Twitter Bot starts.");
+
+var Twit = require('twit')
+var fs = require("fs");
+var config = require("./config");
+
+var T = new Twit(config)
+
+//getLastTweetId();
+//getRetweets("1031995168830959624");
+
+module.exports = {
+	getMove: function () {
+		getLastTweetId();
+	}
+};
+
+function getLastTweetId() {
+  var param = { screen_name: "chessterbot", count: 1};
+  T.get("statuses/user_timeline", param, gotIt);
+
+  function gotIt(err, data, response) {
+    if(err) {
+      console.log(err);
+    } else {
+      // console.log(data);
+      var id = data[0].id_str;
+      // console.log(id + "\nGOT IT!");
+      console.log(id);
+
+      getRetweets(id);
+    }
+  }
+}
+
+function getRetweets(tweetId) {
+  var param = {q: "@chessterbot", count: 50};
+  T.get("search/tweets", param, gotThem);
+
+  function gotThem(err, data, response) {
+  	var arr_comments = new Array();
+    if(err) {
+      console.log(err);
+    } else {
+      // console.log(data.statuses);
+      for(var i = 0 ; i < data.statuses.length; i++) {
+      	if(data.statuses[i].in_reply_to_status_id_str == tweetId) {
+      		//console.log(data.statuses[i].text + "\n");
+      		arr_comments[i] = data.statuses[i].text;
+      	}
+      }
+      
+      if(arr_comments.length != 0) {
+		var counts = countArr(filterArr(arr_comments)); // Array
+		console.log(counts);
+		var max_counts = Math.max.apply(Math, counts.map(function(o) { return o.count; }));
+		var obj = counts.find(function(o){ return o.count == max_counts; })
+		console.log(obj.pos);
+		fs.writeFile("dat", obj.pos, function(err2) {});
+      } else {
+      	getLastTweetId();
+      }
+      
+    }
+  }
+}
+
+function indexOfMax(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var max = arr[0];
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            maxIndex = i;
+            max = arr[i];
+        }
+    }
+
+    return maxIndex;
+}
+
+// [ 'a4', 'Nf4', 'Ka1', 'Ka1', 'Qh8', 'Qh8', 'b6', 'Ka1' ]
+function filterArr(arr) {
+	var arr_str = arr.join();
+	return regex(arr_str);
+}
+
+// [ 'a4', 'Nf4', 'Ka1', 'Ka1', 'Qh8', 'Qh8', 'b6', 'Ka1' ]
+function regex(str) {
+	return str.match(/([KQRBN])?([1-8])?([KQRBN])?([a-h])?([x])?([a-h]){1}([1-8]){1}([+#])?([KQRBN])?|(O-O-O)|(O-O)/g);
+}
+
+// [ { pos: 'a4', count: 1 },
+//   { pos: 'Nf4', count: 1 },
+//   { pos: 'Ka1', count: 3 },
+//   { pos: 'Qh8', count: 2 },
+//   { pos: 'b6', count: 1 } ]
+function countArr(arr) {
+	var arr_str = arr.join();
+	var count = new Array();
+	for(var i = 0; i < arr.length; i++) {
+		var found = count.some(function (elem) {
+			return elem.pos === arr[i];
+		});
+		if(!found) {
+			count.push({pos: arr[i], count: occurrences(arr_str, arr[i], false)});
+		}
+	}
+	return count;
+}
+
+function occurrences(string, subString, allowOverlapping) {
+
+    string += "";
+    subString += "";
+    if (subString.length <= 0) return (string.length + 1);
+
+    var n = 0,
+        pos = 0,
+        step = allowOverlapping ? 1 : subString.length;
+
+    while (true) {
+        pos = string.indexOf(subString, pos);
+        if (pos >= 0) {
+            ++n;
+            pos += step;
+        } else break;
+    }
+    return n;
+}
